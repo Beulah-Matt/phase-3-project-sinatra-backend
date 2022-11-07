c6533ee80dc59db26a734c417ec8f8c215b13ef9
Beulah Mathenge <beulahmathenge@gmail.com>
2022-11-07 18:52:57 +0300

    First GET request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController <Sinatra::Base
-    
+
+    set :default_content_type, 'application/json'
+
+    get '/products' do
+        products = Product.all
+        products.to_json
+    end
 end
