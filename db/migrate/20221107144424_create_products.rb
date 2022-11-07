65b8626a8a893c537d2ffda9f0c674217d851222
Beulah Mathenge <beulahmathenge@gmail.com>
2022-11-07 18:31:27 +0300

    Migrations for products and categories

---
+++ db/migrate/20221107144424_create_products.rb
@@ -0,0 +1,10 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+    t.string :name
+    t.string :image_url
+    t.integer :price
+    t.integer :category_id
+    t.string :description
+  end
+end
