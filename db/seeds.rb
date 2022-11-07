c474b214c9948e89db851fa3c9d7d3a85bde1b22
Beulah Mathenge <beulahmathenge@gmail.com>
2022-11-07 21:18:35 +0300

    Seed data for phones

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,9 @@ Product.destroy_all
 
 puts "Creating categories"
 category1 = Category.create(name: "Fridge")
+category2 = Category.create(name: "Smartphone")
 
-puts "Creating products..."
+puts "Creating fridges..."
 product1 = Product.create(name: "LG French Door", image_url: "https://i.postimg.cc/jS35xfsk/lgfrench.png", 
     price: 300000, category_id: category1.id, 
     description: "LG 570L Slim French Door Refrigerator | Matte Black | InstaView Door-In-Door™
@@ -25,7 +26,27 @@ product4 = Product.create(name: "Samsung Bespoke", image_url: "https://i.postimg
    Digital Inverter Compressor with a 10-Year Warranty |No Frost | All-Around Cooling  
    ")
 
-
+puts "Creating Smartphones..."
+product5 = Product.create(name: "Samsung M53", image_url: "https://i.postimg.cc/rwgH9d2c/samsungm53.jpg",
+    price: 38000, category_id: category2.id, 
+   description: "108MP | 5000mAh | NanoSim | Android 12 OS | 128GB 6GB RAM | 4G
+    ")
+product7 = Product.create(name: "Huawei Nova 7i", image_url: "https://i.postimg.cc/vHKMtSr3/huaweinova71.jpg",
+    price: 37500, category_id: category2.id, 
+    description: "48MP | 4200mAh | NanoSim | Android 10 OS | 128GB 6GB RAM | 4G
+    ")
+product8 = Product.create(name: "Samsung S22 Ultra", image_url: "https://i.postimg.cc/CMt6Py2B/samsung-S22-Ultra.jpg",
+    price: 169000, category_id: category2.id, 
+    description: "50MP | 3700mAh | NanoSim | Android 13 OS | 128GB 6GB RAM | 5G
+    ")
+product9 = Product.create(name: "iPhone 14", image_url: "https://i.postimg.cc/02sHRG8P/i-Phone-14-e-SIM-Design.webp",
+    price: 160000, category_id: category2.id, 
+    description: "Dual 12MP | NanoSim | iOS 16 | 128GB 6GB RAM| 3279 mAh battery | 5G
+    ")
+product10 = Product.create(name: "Redmi Note 10", image_url: "https://i.postimg.cc/cLYqM14L/redmirote10c.jpg",
+    price: 32500, category_id: category2.id, 
+    description: "50MP | 5000mAh | NanoSim | Android 12 OS | 128GB 3GB RAM | LTE
+    ")
 
 
 puts "✅ Done seeding!"
