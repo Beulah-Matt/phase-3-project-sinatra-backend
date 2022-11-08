97ed5a40600b1629ab57158b80947a85be700a1e
Beulah Mathenge <beulahmathenge@gmail.com>
2022-11-08 13:56:24 +0300

    Working delete route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,6 +30,10 @@ class ApplicationController <Sinatra::Base
         product.to_json
     end
 
-
+    delete '/products/:id' do
+        product = Product.find(params[:id])
+        product.destroy
+        product.to_json
+    end
      
 end
